cb37e0a720d7a2b2e8c7f9a82bedcb37c9bf742d
ymkee <puckt0531@gmail.com>
2024-09-02 19:39:59 +0900

    react추가

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const PORT = 8001
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs')
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
+app.use(express.static(path.join(__dirname, 'my-react/build')));
 app.get('/', (req, res) => {
-  res.render('index')
+    res.sendFile(path.join(__dirname, '/my-react/build/index.html'))
 })
 app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`)
