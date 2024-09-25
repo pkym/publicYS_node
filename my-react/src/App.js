813a26714f93e4109b948285a16cd1855b6d29bc
ymkee <puckt0531@gmail.com>
2024-09-25 13:40:27 +0900

    api 연동추가

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  // Provided API key and parameters
+  const apiKey = 'ST9W4WW508Z6XV06';
+  const pageNo = '1';
+  const numOfRows = '10';
+  const crtDt = '20240101';
+  const rgnNm = '1';
+
+  // API endpoint
+  const url = `safeText/V2/api/DSSP-IF-00247?serviceKey=${apiKey}&pageNo=${pageNo}&numOfRows=${numOfRows}`;
+
+
+  useEffect(() => {
+    console.log('Fetching URL:', url);
+    fetch(url)
+      .then((response) => {
+        console.log(response)
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        return response.json(); 
+      })
+      .then((data) => {
+        setData(data); 
+      })
+      .catch((error) => {
+        setError(error.message); 
+      });
+  }, [url]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data Fetcher</h1>
+      {error && <p>Error: {error}</p>}
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre> // Display fetched data
+      ) : (
+        <p>Loading...</p> // Show loading message while fetching
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- web.js
+++ web.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const path = require('path')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express()
 const PORT = 8001
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs')
-// app.get('/', (req, res) => {
-//   res.render('index')
-// })
+// const cors = require('cors');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs')
+// app.use(cors({
+//     origin: "http://localhost:8001",
+//     credentials: true,
+//   })
+// )
+app.use('/safeText', (req, res, next) => {
+    console.log(`Request received: ${req.url}`);
+    next();
+  },createProxyMiddleware({
+    target: 'https://www.safetydata.go.kr',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/safeText': '',
+      },   
+  }));
+
 app.use(express.static(path.join(__dirname, 'my-react/build')));
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/my-react/build/index.html'))
 })
 app.listen(PORT, () => {
